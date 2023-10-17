214573e7f63dc9cb2e80c93fd7bf0ce751d757fa
Irteza7 <m.irtezakhan@hotmail.com>
2023-10-17 23:18:00 +0200

    json cleaning

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,8 +1,10 @@
+import json
 from re import template
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 from config import settings
+from third_parties.linkedin import scrape_linkedin_profile_gist
 
 
 information = """Elon Reeve Musk (born June 28, 1971) is a business magnate and investor. Musk is the founder, chairman, CEO and chief technology officer of SpaceX; angel investor, CEO, product architect and former chairman of Tesla, Inc.; owner, chairman and CTO of X Corp.; founder of the Boring Company; co-founder of Neuralink and OpenAI; and president of the Musk Foundation. He is the wealthiest person in the world, with an estimated net worth of US$232 billion as of September 2023, according to the Bloomberg Billionaires Index, and $253 billion according to Forbes, primarily from his ownership stakes in both Tesla and SpaceX.[4][5]
@@ -13,17 +15,20 @@ Musk was born in Pretoria, South Africa, and briefly attended the University of 
 if __name__ == "__main__":
     print('Hello LangChain')
 
-    summary_template = """
-        given the information {information} about a person from I want you to create:
-        1. a short summary 50 words
-        2. two interesting facts about them (keep it in three four words)
-    """
+    # summary_template = """
+    #     given the information {information} about a person from I want you to create:
+    #     1. a short summary 50 words
+    #     2. two interesting facts about them (keep it in three four words)
+    # """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    # summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
-    llm = ChatOpenAI(temperature=0.2, model="gpt-3.5-turbo", openai_api_key=settings.openai_api_key)
+    # llm = ChatOpenAI(temperature=0.2, model="gpt-3.5-turbo", openai_api_key=settings.openai_api_key)
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(information=information))
+    # print(chain.run(information=information))
 
+    linkdin_data = scrape_linkedin_profile_gist('https://gist.githubusercontent.com/Irteza7/16ba2fa6b8f9f5b00c95a2d139f428e7/raw/fda63aab2a0a9ce533aec555f0332710984db4de/gistfile1.txt')
+    linkdin_data_json = json.dumps(linkdin_data)
+    print(linkdin_data_json)
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -1,12 +1,83 @@
 import requests
 from config import settings
 
-def scrape_linkedin(linkdin_url):
+def scrape_linkedin_profile(linkdin_url):
+    """Scrape information from LinkedIn profiles,
+    Manually scrape information from LinkedIn profile"""
+
     api_key = settings.proxycurl_api
     headers = {'Authorization': 'Bearer ' + api_key}
     api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
-    params = {'linkedin_profile_url': linkdin_url}
+    params = {'linkedin_profile_url': linkdin_url, 'use_cache': 'if-recent'}
 
     response = requests.get(api_endpoint,
                         params=params,
                         headers=headers)
+    
+    data = response.json()
+    data = {
+        k: v 
+        for k, v in data.items()
+        if v not in ([], "", '', 'null', 'None', None)
+            and k not in ["people_also_viewed", "certifications", "similarly_named_profiles", "profile_pic_url"]
+    }
+
+    if data.get("groups"):
+        for groups_dict in data.get("groups"):
+            groups_dict.pop("profile_pic_url")
+
+    return data
+
+
+def scrape_linkedin_profile_gist(gist_url):
+    """Scrape information from LinkedIn profiles,
+    Manually scrape information from LinkedIn profile"""
+
+    response = requests.get(gist_url)
+    
+    data = response.json()
+    data = {
+        k: v 
+        for k, v in data.items()
+        if v not in ([], "", '', None, 'null', 'None')
+            and k not in ["people_also_viewed", "certifications", "similarly_named_profiles", "profile_pic_url"]
+    }
+
+    # Removing fields with values of 'None' and URL fields from each dictionary
+    if "experiences" in data:
+        for experience in data["experiences"]:
+            # Removing keys with 'None' values
+            keys_to_remove = [key for key, value in experience.items() if value == "None"]
+            for key in keys_to_remove:
+                experience.pop(key, None)
+
+            # Removing URL fields         
+            experience.pop("logo_url", None)
+            experience.pop("company_linkedin_profile_url", None)
+    
+    if "education" in data:
+        for edu in data["education"]:
+            # Removing keys with 'None' values
+            keys_to_remove = [key for key, value in edu.items() if value == "None"]
+            for key in keys_to_remove:
+                edu.pop(key, None)
+            
+            # Removing URL fields
+            edu.pop("school_linkedin_profile_url", None)
+            edu.pop("logo_url", None)
+
+    if "accomplishment_projects" in data:
+        for projects in data["accomplishment_projects"]:
+            # Removing keys with 'None' values
+            keys_to_remove = [key for key, value in projects.items() if value == "None"]
+            for key in keys_to_remove:
+                projects.pop(key, None)
+            
+            # Removing URL fields
+            projects.pop("url", None)
+
+    if data.get("groups"):
+        for groups_dict in data.get("groups"):
+            groups_dict.pop("profile_pic_url", None)
+
+    return data
